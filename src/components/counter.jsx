e5b72322f1aa4852d4dbf959d159a503a5ea1fc1
Vadim Moroz <v9858480345@yandex.ru>
2021-12-08 10:54:16 +0300

    add button reset

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,13 +10,13 @@ const Counter = (props) =>{
     classes += value === 0? "bg-warning":"bg-primary"
     return classes
   }
- 
+
   return (
     <div>
       <span>{props.name}</span>
       <span className={changeClasses()}>{formatValue()}</span>
       <button className='btn btn-primary btn-sm m-2' onClick={()=>props.onIncrement(props.id)}>+</button>
-      <button className='btn btn-primary btn-sm m-2' onClick={props.onDecrement}>-</button>
+      <button className='btn btn-primary btn-sm m-2' onClick={()=>props.onDecrement(props.id)}>-</button>
       <button className='btn btn-danger btn-sm m-2' onClick={()=> props.onDelete(props.id)}>Delete</button>
     </div>
     
--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -2,14 +2,14 @@ import React,{useState} from 'react'
 import Counter from './counter'
 
 const CountersList = () => {
-const initialState = [
-  {id: 0, value: 0, name:'Ненужная вещь'},
-  {id: 1, value: 4, name:'Ложка'},
-  {id: 2, value: 0, name:'Вилка'},
-  {id: 3, value: 0, name:'Тарелка'},
-  {id: 4, value: 0, name:'Набор минималиста'}
-]
-const [counters, setCounters] = useState(initialState)
+  const initialState = [
+    {id: 0, value: 0, name:'Ненужная вещь'},
+    {id: 1, value: 4, name:'Ложка'},
+    {id: 2, value: 0, name:'Вилка'},
+    {id: 3, value: 0, name:'Тарелка'},
+    {id: 4, value: 0, name:'Набор минималиста'}
+  ]
+  const [counters, setCounters] = useState(initialState)
   const handleDelet = (id) => {
     const newCounter = counters.filter(count => count.id !== id)
     setCounters(newCounter)
@@ -18,40 +18,42 @@ const [counters, setCounters] = useState(initialState)
     setCounters(initialState)
     console.log( 'handleReset');
   }
-const handleIncrement = (id) => { 
-  let clickIncrement = counters.map(item => {
-  if (item.id !== id) {
-    console.log(item)
-  return item  // здесь массив не измененных объектов
-  }else{
-    item.value = item.value +1
-    let newitem = item // здесь массив  измененного объекта
-    console.log(newitem)
-    return newitem
+  const handleIncrement = (id) => { 
+    let clickIncrement = counters.map(item => {
+      if (item.id !== id) {
+        return item  // здесь массив не измененных объектов
+      }else{
+        item.value = item.value +1
+        return item  // здесь массив  измененного объекта
+      }
+    })
+    setCounters(clickIncrement)
   }
-  //как их соеденить в один массив чтобы потом передать setCounters(clickIncrement)
-  
- })
- //setCounters(clickIncrement)
-  }
-  const handleDecrement = () => {
-    console.log('handleDecrement');
-  //  setCounters(prevState => prevState -1)
+  const handleDecrement = (id) => {
+    let clickDecrement = counters.map(item => {
+      if (item.id !== id) {
+        return item  
+      }else{
+        item.value = item.value -1
+        return item
+      }
+    })
+    setCounters(clickDecrement)
   }
   return(
     <>
-    {counters.map(counter =>
-    <Counter 
-    key={counter.id}
-    {...counter}
-    onDelete={handleDelet}
-    onIncrement={handleIncrement}
-    onDecrement={handleDecrement}
-    />)}
-    <button 
-    className="btn btn-primary btn-sm m-2" 
-    onClick={handleReset}
-    >Сброс</button>
+      {counters.map(counter =>
+      <Counter 
+        key={counter.id}
+        {...counter}
+        onDelete={handleDelet}
+        onIncrement={handleIncrement}
+        onDecrement={handleDecrement}
+      />)}
+      <button 
+        className="btn btn-primary btn-sm m-2" 
+        onClick={handleReset}
+      >Сброс</button>
     </>
   )
 }
